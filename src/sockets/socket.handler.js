59c1d87cabb478f8cc420bfce5ae55dc0b4414d1
unknown <sachinkhatri53@gmail.com>
2024-10-24 23:16:28 +0545

    updated origins for CORS

--- src/sockets/socket.handler.js
+++ src/sockets/socket.handler.js
@@ -3,8 +3,15 @@ let io;
 export const setupWebSocket = (server) => {
   io = new Server(server, {
     cors: {
-      origin: "http://localhost:5173",
-      methods: ["GET", "POST"],
+      origin: [
+        process.env.CLIENT_DEVELOPMENT_URL,
+        process.env.CLIENT_USER_URL,
+        process.env.CLIENT_OWNER_URL,
+        process.env.VERCEL_CLIENT_USER_URL,
+        process.env.NETLIFY_CLIENT_USER_URL,
+        process.env.NETLIFY_CLIENT_OWNER_URL,
+      ],
+      methods: ["GET", "POST", "PUT", "DELETE"],
     },
   });
   io.on("connection", (socket) => {
